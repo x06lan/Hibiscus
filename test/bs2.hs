75ef00ac87825e9375d1965ca00ada302e61a883
rick <t110590016@ntut.edu.tw>
2024-12-05 17:47:16 +0800

    :D

--- test/bs2.hs
+++ test/bs2.hs
@@ -32,27 +32,26 @@ update env x@(n, _) =
 
 type Subst a = [(Symbol, Type a)]
 
-getNewSymbol :: (Show a) => Env a -> a -> Type a
-getNewSymbol env a =
+getNewUnkTy :: (Show a) => Env a -> a -> Type a
+getNewUnkTy env a =
   let
     extractIndex (_, TUnknown _ i) = i
     extractIndex _ = 0
     nextIndex = maximum (0 : map extractIndex env) + 1
-    t' = TUnknown a nextIndex
-   in
-    t'
+  in
+    TUnknown a nextIndex
 
 getDecType :: (Show a) => Env a -> Dec a -> Result (Env a, Type a)
 getDecType env_ (Dec a name args _) =
   let
-    tb = getNewSymbol env_ a
+    tb = getNewUnkTy env_ a
   in do
     foldrM h (env_, tb) args
   where
 --  h :: Arg -> (Env, Type) -> Result (Env, Type)
     h (Argument a name) (e, tb) =
       let
-        ta = getNewSymbol e a
+        ta = getNewUnkTy e a
       in do
         e' <- update e (name, ta)
         return (e', TArrow a ta tb)
@@ -94,27 +93,24 @@ typeInfer env expr_ = case expr_ of
      do
        e0 <- typeCheck env [dec]
        typeInfer e0 expr
---      let
---        argEnv = concatMap getConstraints args
---      in do
---        (s1, t1) <- typeInfer (env ++ argEnv) decexp
---        (s2, t2) <- typeInfer (env ++ [(name, t1)]) exp
---        return t2
---    EApp a f x ->
---      do
---        -- tf = tx -> tv
---        (s1, tf) <- typeInfer env f
---        (s2, tx) <- typeInfer (applySubstEnv s1 env) x
---        tv <- undefined -- TODO: get new type var
---        s3 <- unify (applySubst s2 tf) (TArrow a tx tv)
---        return undefined
---      return (s3 ++ s2 ++ s1, applySubst s3 tv)
+    EApp a f x ->
+      do
+        -- tf = tx -> tv
+        (s1, tf) <- typeInfer env f -- FIXME: need subst
+        (s2, tx) <- typeInfer (subEnv s1 env) x
+        return (getNewUnkTy (subEnv (s1 ++ s2) env) a)
+          >>= \tv ->
+            unify env (subTy (s1 ++ s2) tf) (TArrow a tx tv)
+              >>= \s3 ->
+                return (s1 ++ s2 ++ s3, subTy (s1 ++ s2 ++ s3) tv)
+    EBinOp a e1 op e2 -> return ([], getNewUnkTy env a) -- TODO: biop
+    EOp a _ -> return ([], getNewUnkTy env a) -- TODO: op
     EPar _ exp -> typeInfer env exp
     EInt a _    -> return ([], litType a "Int"   )
     EFloat a _  -> return ([], litType a "Float" )
     EString a _ -> return ([], litType a "String")
-    EUnit a     -> return ([], TUnit a       )
-    _ -> Left "WIP: unsupported structure"
+    EUnit a     -> return ([], TUnit a           )
+    x -> Left $ "WIP: unsupported structure: " ++ show x
 
 typeCheck :: (Show a) => Env a -> [Dec a] -> Result (Env a)
 typeCheck env_ decs_ = foldlM h env_ decs_
@@ -134,10 +130,11 @@ typeCheck env_ decs_ = foldlM h env_ decs_
             return (subEnv s2 env, subTy s2 decT)
           Nothing -> update env (name, decT) >>= \e -> return (e, decT)
         (s2, t2) <- typeInfer innerEnv expr -- FIXME: need subst
-        unify eee realT t2
+        unify eee realT (TArrow a (getNewUnkTy eee a) t2)
         return eee
 
 main :: IO ()
 main = do
   content <- BS.readFile "./example/test.hi"
+  print $                  runAlex content parseHibiscus
   print $ typeCheck [] =<< runAlex content parseHibiscus
