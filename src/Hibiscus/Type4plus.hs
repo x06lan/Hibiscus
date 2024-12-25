f8656a3b97a401f959f08de0b88d86fd11598bb9
rick <t110590016@ntut.edu.tw>
2024-12-26 01:16:45 +0800

    works!

--- src/Hibiscus/Type4plus.hs
+++ src/Hibiscus/Type4plus.hs
@@ -17,12 +17,19 @@ import Data.Tuple (curry)
 import Prelude hiding (lookup)
 
 import GHC.Stack (HasCallStack)
-import Debug.Trace (traceStack)
+import Debug.Trace hiding (trace)
+
+trace :: String -> a -> a
+trace _ = id
 
 type TypeEnv = Map.Map (Name ()) (Type ()) 
-type Subst = Map.Map MetaSymbol (Type ()) -- responsible to maintain all metas
+newtype Subst = Subst (Map.Map MetaSymbol (Type ())) deriving (Show) -- responsible to maintain all metas
 type Context = (TypeEnv, Subst)
 
+instance Semigroup Subst where
+  s1@(Subst map1) <> (Subst map2) = Subst $ Map.map (applySub s1) map2 `Map.union` map1
+instance Monoid Subst where
+  mempty = Subst mempty
 lookup :: (Name a) -> TypeEnv -> Maybe (Type ())
 lookup n env = Map.lookup (void n) env
 
@@ -33,21 +40,27 @@ instance MonadFail Result where
 class Substable a where
   applySub :: Subst -> a -> a
 instance Substable (Type ()) where
-  applySub sub t@(TUnknown _ n) = fromMaybe t (Map.lookup n sub)
+  applySub (Subst sub) t@(TUnknown _ n) = 
+    trace ("applySub: " ++ show sub) $ 
+    fromMaybe t (Map.lookup n sub)
+  applySub s (TPar _ t) = applySub s t
+  applySub s (TArrow _ ta tb) = TArrow () ta' tb'
+    where
+      ta' = applySub s ta
+      tb' = applySub s tb
   applySub _ t = t
 
 literalT :: String -> Type ()
 literalT = TVar () . Name () . pack
 
-
 bindVar :: MetaSymbol -> Type () -> Result Subst
-bindVar v t = return $ Map.fromList [(v,t)]
+bindVar v t = return $ Subst $ Map.fromList [(v,t)]
 
 unify :: HasCallStack => Type () -> Type () -> Result Subst
 unify t1 t2
   | t1 == t2 = return mempty
   | otherwise = 
-      traceStack ("unifying: " ++ show t1 ++ " ==? " ++ show t2) $ 
+      trace ("unifying: " ++ show t1 ++ " ==? " ++ show t2) $ 
       case (t1, t2) of
         (TUnknown _ v, t) -> bindVar v t
         (t, TUnknown _ v) -> bindVar v t
@@ -58,7 +71,7 @@ unify t1 t2
         _ -> error $ "Cannot unify " ++ show t1 ++ " with " ++ show t2
 
 freshTypeUnk :: Subst -> (Subst, Type ())
-freshTypeUnk subs = (Map.fromList [(newSym, t')], t')
+freshTypeUnk (Subst subs) = (Subst $ Map.fromList [(newSym, t')], t')
   where
     lastone = maximum $ [0] ++ Map.keys subs
     newSym = 1 + lastone
@@ -144,6 +157,18 @@ inferExpr ctx@(env,sub) expr = case expr of
       let ctx'' = (fmap (applySub s1) e0, s1 <> s0)
       (s2, body') <- inferExpr ctx'' body
       return (s2 <> s1, ELetIn (a, getType body') decs' body')
+  EApp a f x -> do
+      -- tf = tx -> tv
+      (s1, f') <- inferExpr ctx              f
+      (s2, x') <- inferExpr (env, s1 <> sub) x
+      let tf = getType f'
+      let tx = getType x'
+      let (s3, tv) = freshTypeUnk (s2 <> s1 <> sub)
+      let s3210 = s3 <> s2 <> s1 <> sub
+      s4 <- unify (applySub s3210 tf) (TArrow () tx tv)
+      let s43210 = s4 <> s3210
+      let finalEApp = EApp (a, applySub s4 tv) (applySubM s43210 f') (applySubM s43210 x')
+      return (s43210, finalEApp)
   EBinOp _ e1 _ e2 ->
     do
       (s1, e1') <- inferExpr ctx              e1
@@ -153,7 +178,6 @@ inferExpr ctx@(env,sub) expr = case expr of
       s3 <- unify t1 t2
       let finalSub = s3 <> s2 <> s1
       return (finalSub, addType (applySub s3 t1) expr)
-    
   _ -> return (s1, addType t expr)
     where
       (s1, t) = freshTypeUnk sub
@@ -166,22 +190,22 @@ inferDecs (env, sub) = foldlM aux (sub, [])
     aux (s0, decs) (Dec a name args body) = do
       let (s1, bodyType) = freshTypeUnk s0
       let (s2, argWithTypes) = magic (s1 <> s0) args
-      let s210 = s2 <> s1 <> s0
+      let s2s1s0 = s2 <> s1 <> s0
       let funcType = foldr (TArrow ()) bodyType (map getType argWithTypes)
       let innerEnv = argToEnv argWithTypes
-      (s4, body') <- inferExpr (innerEnv <> env, s210) body
+      (s4, body') <- inferExpr (innerEnv <> env, s2s1s0) body
       let bodyType' = getType body'
       s5 <- unify bodyType' bodyType
       let finalBody = applySubM s5 body'
-      let funcType' = applySub (s5 <> s4 <> s210) funcType
-      let finalArgs = map (applySubM (s5 <> s4 <> s210)) argWithTypes
+      let funcType' = applySub (s5 <> s4 <> s2s1s0) funcType
+      let prefinalType = applySub (s5 <> s4 <> s2s1s0) $ fromJust $ lookup name env
+      s6 <- unify prefinalType funcType'
+      let finalType = applySub s6 funcType'
+      let finalArgs = map (applySubM (s6 <> s5 <> s4 <> s2s1s0)) argWithTypes
       let finalName = addType (TUnit ()) name
-      -- TODO: unify with annotation
-      let finalSub = s5 <> s4 <> s210
-      let finalDec = Dec (a, funcType') finalName finalArgs finalBody
-      return (finalSub, finalDec : (map (applySubM finalSub) decs))
-    
-    -- Handle the case where the declaration is not defination
+      let finalSub = s6 <> s5 <> s4 <> s2s1s0
+      let finalDec = Dec (a, finalType) finalName finalArgs finalBody
+      return (finalSub, map (applySubM finalSub) (finalDec : decs))
     aux microencourage _ = return microencourage
 
 infer :: [Dec a] -> Result [Dec (a, Type ())]
