d4c9178c42dfb7b744ad0f1dd3b51f81b8a474fa
rick <t110590016@ntut.edu.tw>
2025-01-05 23:43:16 +0800

    add a simple test

---
+++ test/test.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import System.Exit
+
+import qualified Data.ByteString.Lazy.Char8 as BS
+import Data.Either (fromRight)
+import Hibiscus.CodeGen (generate)
+import Hibiscus.CodeGen.Constants (defaultConfig)
+import Hibiscus.Parsing.Lexer (runAlex)
+import Hibiscus.Parsing.Parser (parseHibiscus)
+import Hibiscus.TypeInfer (infer)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  content <- BS.readFile "test/test.hi"
+  let parseResult = fromRight (error "parse error") $ runAlex content parseHibiscus
+  let inferResult = fromRight (error "infer error") $ infer parseResult
+  let asmResult = generate defaultConfig inferResult
+  return ()
